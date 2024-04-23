cb6450e9f46a8410fe811b92fa30036948027d2b
andydnh <andrescuevanw@gmail.com>
2024-04-22 23:30:58 -0500

    Se agrega el proyecto

---
+++ Proyecto001/src/base/Main.java
@@ -0,0 +1,114 @@
+package base;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.DisplayMode;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+
+import javax.swing.AbstractAction;
+import javax.swing.ActionMap;
+import javax.swing.InputMap;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.KeyStroke;
+
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLCapabilities;
+import com.jogamp.opengl.GLEventListener;
+import com.jogamp.opengl.GLProfile;
+import com.jogamp.opengl.util.FPSAnimator;
+
+public class Main  extends JFrame implements GLEventListener{
+	
+	private static GraphicsEnvironment graphicsEnviorment;
+	private static boolean isFullScreen = false;
+	public static DisplayMode dm, dm_old;
+	private static Dimension xgraphic;
+	private static Point point = new Point(0, 0);
+
+	public static void main(String[] args) {
+		final GLProfile profile = GLProfile.get(GLProfile.GL2);
+		GLCapabilities capabilities = new GLCapabilities(profile);
+		
+		// The canvas 
+		final GLCanvas glcanvas = new GLCanvas(capabilities);
+		Render r = new Render();
+		glcanvas.addGLEventListener(r);
+		glcanvas.setSize(400, 400);
+		
+		final FPSAnimator animator = new FPSAnimator(glcanvas, 300,true );
+		
+		final JFrame frame = new JFrame ("nehe: Lesson 1");
+		
+		frame.getContentPane().add(glcanvas);
+		
+		//Shutdown
+		frame.addWindowListener(new WindowAdapter(){
+			public void windowClosing(WindowEvent e){
+				if(animator.isStarted())
+					animator.stop();
+				System.exit(0);
+			}
+		});
+		
+		frame.setSize(frame.getContentPane().getPreferredSize());
+		/**
+		 * Centers the screen on start up
+		 * 
+		 */
+		graphicsEnviorment = GraphicsEnvironment.getLocalGraphicsEnvironment();
+
+		GraphicsDevice[] devices = graphicsEnviorment.getScreenDevices();
+
+		dm_old = devices[0].getDisplayMode();
+		dm = dm_old;
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+		int windowX = Math.max(0, (screenSize.width - frame.getWidth()) / 2);
+		int windowY = Math.max(0, (screenSize.height - frame.getHeight()) / 2);
+
+		frame.setLocation(windowX, windowY);
+		/**
+				 * 
+				 */
+		frame.setVisible(true);
+		/*
+		 * Time to add Button Control
+		 */
+		JPanel p = new JPanel();
+		p.setPreferredSize(new Dimension(0,0));
+		frame.add(p, BorderLayout.SOUTH);
+		
+		keyBindings(p, frame, r);
+		animator.start();
+	}
+
+	public void init(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void dispose(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void display(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
